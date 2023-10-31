624491ddefc6c79c55c2dd22353a0bb5e59e488c
zharris915 <zharrris915@yahoo.com>
2023-10-30 21:33:06 -0600

    updated index js

--- index.js
+++ index.js
@@ -19,13 +19,41 @@ app.get('/api', (req, res) => {
 });
 
 // get all pets from the database
-app.get('/api/v1/pets', (req, res) => {
+app.get('/api/v1/pets', async (req, res) => {
+        try {
+            const allPets = await Pet.find({});
+            res.json(allPets);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
     // send the pets array as a response
 
-});
 
 // get pet by owner with query string
-app.get('/api/v1/pets/owner', (req, res) => {
+// Assuming you have a database and a function to retrieve a pet by owner's name
+
+// Example of retrieving a pet by owner's name from the database (pseudocode)
+app.get('/api/v1/pets/owner', async (req, res) => {
+    const owner = req.query.owner;
+
+    try {
+        // Query the database to find a pet by owner's name
+        const pet = await Pet.findOne({ owner }); 
+
+        if (pet) {
+            // Return the pet details as a JSON response
+            res.json(pet);
+        } else {
+            // If the pet is not found, return a 404 status with a relevant message
+            res.status(404).json({ message: 'Pet with this owner not found' });
+        }
+    } catch (error) {
+        // Handle errors, if any
+        res.status(500).json({ error: error.message });
+    }
+});
+
     // get the owner from the request
 
 
@@ -37,7 +65,26 @@ app.get('/api/v1/pets/owner', (req, res) => {
 });
 
 // get pet by name
-app.get('/api/v1/pets/:name', (req, res) => {
+app.get('/api/v1/pets/:name', async (req, res) => {
+            const name = req.params.name;
+    
+        try {
+            // Query the database to find a pet by name
+            const pet = await Pet.findOne({ name }); 
+    
+            if (pet) {
+                // Return the pet details as a JSON response
+                res.json(pet);
+            } else {
+                // If the pet is not found, return a 404 status with a relevant message
+                res.status(404).json({ message: 'Pet not found' });
+            }
+        } catch (error) {
+            // Handle errors, if any
+            res.status(500).json({ error: error.message });
+        }
+    });
+    
     // get the name from the request
 
 
@@ -46,7 +93,7 @@ app.get('/api/v1/pets/:name', (req, res) => {
 
     // send the pet as a response
 
-});
+
 
 app.listen(PORT, () => {
     console.log('Server is listening on port ' + PORT);
